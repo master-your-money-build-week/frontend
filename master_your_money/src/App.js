aea4fda335e9565fee5d677469494cc49e622471
Kylevb405 <kylevanburen405@gmail.com>
2019-08-25 21:36:47 -0400

    loginPage 2.0

--- master_your_money/src/App.js
+++ master_your_money/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import GoalList from './components/GoalList'
+import LoginPage from './components/LoginPage'
 import './App.css';
 
 function App() {
--- master_your_money/src/components/LoginPage.js
+++ master_your_money/src/components/LoginPage.js
@@ -2,11 +2,29 @@ import React from "react";
 
 export default function LoginPage() {
     return (
-        <section className="welcome-page">
+        <section className="Login-page">
             <header>
-                <h1>Welcome to the ultimate fan site!</h1>
-                <img></img>
+                <h1>Welcome Back!</h1>
             </header>
+            <div>
+                <label>Username:</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type="password" />
+            </div>
+            <div>
+                <input type="checkbox" />
+            </div>
+            <button>
+                <label>Remember Me</label>
+                <h1>Sign in</h1>
+            </button>
+            <div>By clicking Sign in, you agree to our Terms and have read and aacknowledge our Privacy Statement</div>
+            <div>Forgot Username/Password?</div>
+            <span>No account yet?</span>
+            <span>Create an account</span>
         </section>
     );
 }
