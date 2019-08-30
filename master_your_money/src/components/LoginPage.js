b60e01461ed08e21f02151856091d4ef013f281a
dmunter2 <devon.munter@gmail.com>
2019-08-30 04:58:14 -0700

    css login page

--- master_your_money/src/components/LoginPage.js
+++ master_your_money/src/components/LoginPage.js
@@ -36,10 +36,17 @@ const BOTTOMDIV = styled.div`
 display: flex;
 flex-direction: row;
 margin-left: 10%;
+margin-top: 1%;
+margin-bottom: 1%;
 width: 80%;
 `
 const DIV4 = styled.div`
 margin-right: 2%;
+display: flex;
+flex-direction: row;
+align-content: center;
+width: 12%;
+justify-content: space-around;
 `
 
 const DIV5 = styled.div`
@@ -49,13 +56,20 @@ margin-right: 2%;
 
 const BTN = styled.button`
 margin-right: 2%;
-
-width: 100px;
+background-color: #1DB954;
+width: 7%;
+height: 30px;
+border-radius: 10%;
+border: none;
+color: white;
 `
 const LOWDIV = styled.div`
 margin-left: 10%;
 
 `
+const LABEL = styled.label`
+`
+
 const SECTIONDIV = styled.div`
 background: #E8E8E8;
 width: 100%;
@@ -73,25 +87,23 @@ export default function LoginPage() {
                 <h1>Welcome Back!</h1>
             </header>
             <DIVI>
-                <INPUT type="text" placeholder='Phone or username'/>
+                <INPUT className='inputfocus' type="text" placeholder='Phone or username'/>
             </DIVI>
             <DIVI>
-                <INPUT type="text" placeholder='Password'/>
+                <INPUT className='inputfocus' type="text" placeholder='Password'/>
             </DIVI>
         </SECTION>
 
         <SECTIONDIV>
 
             <BOTTOMDIV>
-                <BTN>
-                    <a href="/ParentPage/welcome" >Sign in</a>
-                </BTN>
+                <BTN href="/ParentPage/welcome" >Sign in</BTN>
                 <DIV4>
                     <input type="checkbox" />
-                    <label>Remember Me</label>
+                    <LABEL>Remember Me</LABEL>
                 </DIV4>
                 <DIV5>
-                    <div>Forgot Username/Password?</div>
+                    <div className='forgot-user'>Forgot Username/Password?</div>
                 </DIV5>
             </BOTTOMDIV>
             
